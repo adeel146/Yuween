15f9aaeab9589074188994f1c4c288cd07c85d02
adeel146 <adeelmuhammad146@gmail.com>
2023-07-12 22:44:37 +0500

    management responsive

--- src/components/AdminComponents/management/vihicalRenterSteps/ManagemntIndex.jsx
+++ src/components/AdminComponents/management/vihicalRenterSteps/ManagemntIndex.jsx
@@ -19,7 +19,18 @@ const ManagementIndex = () => {
   );
   return (
     <>
-      <div className="flex lg:flex-nowrap  flex-wrap  items-center   mt-3">
+      <select
+        className="border-0 shadow-[0_4px_20px_0px_rgba(0,0,0,0.08)] rounded-md bg-white px-2 xl:hidden md:hidden w-full py-2 focus:outline-none mt-2"
+        onChange={(e) => dispatch(UpdateManagementIndex(e.target.value))}
+      >
+        <option value="">Please Select</option>
+        {list.map((list, i) => (
+          <option value={i} key={i}>
+            {list}
+          </option>
+        ))}
+      </select>
+      <div className="xl:flex md:flex hidden justify-between items-center border-b-2  mt-3">
         {list.map((list, i) => (
           <h1
             className={`text-center cursor-pointer text-[#ACACAC] py-2 lg:px-2.5 px-2.5 mr-5 truncate  ${
